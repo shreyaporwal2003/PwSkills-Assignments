396f86b804f12993639b6c830605e4fe9613d53d
shreyaporwal2003 <shreyaporwal167@gmail.com>
2024-09-05 00:53:32 +0530

    second commit

--- SortingArray/BubbleSort.java
+++ SortingArray/BubbleSort.java
@@ -1,10 +1,9 @@
-
 public class BubbleSort{
     public static void main(String[] args) {
         int[] arr = {3, 5, 1, 6, 0};
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
-               
+
                 if (arr[j] < arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
